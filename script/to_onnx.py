9e79006da20255fc1482387b01b874d0ab43dcb0
HHgzs <3166237657@qq.com>
2024-05-29 23:41:50 +0800

    修复onnx导出

--- script/to_onnx.py
+++ script/to_onnx.py
@@ -3,9 +3,7 @@ import torch.onnx
 from networks.resnet import resnet18
 import onnx
 
-def convert_pth_to_onnx(pth_path, onnx_path, num_classes=8):
-    # 初始化模型
-    model = resnet18(num_classes=num_classes)
+def convert_pth_to_onnx(pth_path, onnx_path, model):
     
     # 加载模型权重
     model.load_state_dict(torch.load(pth_path, map_location='cpu'))
@@ -31,6 +29,9 @@ def convert_pth_to_onnx(pth_path, onnx_path, num_classes=8):
         print(f"Error occurred while converting to ONNX: {e}")
 
 if __name__ == '__main__':
-    pth_path = './models/model_epoch_8_acc_99.03.pth'  # 这里替换为你实际的模型路径
-    onnx_path = './models/model.onnx'  # 这里替换为你想要保存ONNX模型的路径
-    convert_pth_to_onnx(pth_path, onnx_path)
+    
+    pth_path = './models/model_epoch_8_acc_99.03.pth'
+    onnx_path = './outputs/classify_resnet18_epoch8.onnx'
+    model = resnet18(num_classes=8)
+    
+    convert_pth_to_onnx(pth_path, onnx_path, model)
